2c888fadea9ce9e78c1791c1a6a5bd18021d7dd8
CillyDev <abdulaziz.nabaa@gmail.com>
2025-07-03 14:07:22 +0200

    added error handling

--- practice.go
+++ practice.go
@@ -1,9 +1,16 @@
 package something
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Something(name string) string {
+func Something(name string) (string, error) {
 	message := fmt.Sprintf("Hi, %v. Welcome to Chilli's!", name)
-	fmt.Println(message)
-	return (message)
+	if message == "" {
+		return "", errors.New("you forgot the name dumbass")
+
+	}
+
+	return message, nil
 }
